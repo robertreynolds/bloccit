be1c78a26db1312586c9fbcf568f0feeb3ae6b6a
Robert Reynolds <robert.j.reynolds@gmail.com>
2014-02-07 16:47:57 +0000

    Associated and nested topics and posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,24 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
   def show
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
     authorize! :create, Post, message: "You need to be a member to create a new post."
   end
 
   def edit
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :edit, @post, message: "You need to own the post to edit it."
   end
 
   def create
+    @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params[:post])
     authorize! :create, @post, message: "You need to be signed in to do that."
       if @post.save
@@ -30,6 +31,7 @@ class PostsController < ApplicationController
     end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :update, @post, message: "You need to own the post to edit it."
       if @post.update_attributes(params[:post])
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ActiveRecord::Base
-  attr_accessible :body, :title
+  attr_accessible :body, :title, :topic
   has_many :comments
   belongs_to :user
+  belongs_to :topic
 
   default_scope order('created_at DESC')
+end
+
+class Topic < ActiveRecord::Base
+  attr_accessible :description, :name, :public
+  has_many :posts
 end
