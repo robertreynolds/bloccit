030d2eea53756dff61ca3510986091b6613c133b
Robert Reynolds <robert.j.reynolds@gmail.com>
2014-02-07 15:43:30 +0000

    Implemented Cancan and basic roles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,17 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    authorize! :create, Post, message: "You need to be a member to create a new post."
   end
 
   def edit
     @post = Post.find(params[:id])
+    authorize! :edit, @post, message: "You need to own the post to edit it."
   end
 
   def create
     @post = current_user.posts.build(params[:post])
+    authorize! :create, @post, message: "You need to be signed in to do that."
       if @post.save
         flash[:notice] = "Post was saved."
         redirect_to @post
@@ -28,6 +31,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    authorize! :update, @post, message: "You need to own the post to edit it."
       if @post.update_attributes(params[:post])
         flash[:notice] = "Post was updated."
         redirect_to @post
