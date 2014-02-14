9089e4ff0efacffe30d29f5d4661337ab00b0bbc
Robert Reynolds <robert.j.reynolds@gmail.com>
2014-02-14 14:17:42 +0000

    Added avatars for users

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,7 +10,7 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
-    @posts = @topic.posts #Posts.find(params[:post_id]) 
+    @posts = @topic.posts 
   end
 
   def edit
